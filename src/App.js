c212264ebdbcd79778d28f173e2ab1ee032d64d8
ABEL-DAS-UPN <n00254384@upn.pe>
2023-04-13 22:06:55 -0500

    Se agregó nuevos campos en App.js para el examen T1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,127 @@
-import logo from './logo.svg';
+import React, { useReducer, useState } from 'react';
 import './App.css';
 
+const formReducer = (state, event) => {
+ return {
+   ...state,
+   [event.name]: event.value
+ }
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [razonSocial, setRazonSocial] = useReducer(formReducer, {});
+  const [nombreComercial, setNombreComercial] = useReducer(formReducer, {});
+  const [ruc, setRUC] = useReducer(formReducer, {});
+  const [contacto, setContacto] = useReducer(formReducer, {});
+  const [telefono, setTelefono] = useReducer(formReducer, {});
+  const [submitting, setSubmitting] = useState(false);
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    setSubmitting(true);
+
+    setTimeout(() => {
+      setSubmitting(false);
+    }, 20000);
+  }
+
+  const handleChange = event => {
+    setRazonSocial({
+      name: event.target.name,
+      value: event.target.value,
+    });
+  }
+
+  const handleChange2 = event => {
+    setNombreComercial({
+      name: event.target.name,
+      value: event.target.value,
+    });
+  }
+
+  const handleChange3 = event => {
+    setRUC({
+      name: event.target.name,
+      value: event.target.value,
+    });
+  }
+
+  const handleChange4 = event => {
+    setContacto({
+      name: event.target.name,
+      value: event.target.value,
+    });
+  }
+
+  const handleChange5 = event => {
+    setTelefono({
+      name: event.target.name,
+      value: event.target.value,
+    });
+  }
+  return(
+    <div className="estilo">
+      <h1>Registro de Proveedor</h1>
+      {submitting &&
+       <div>
+         Información de proveedor registrada
+         <ul>
+           {Object.entries(razonSocial).map(([name, value]) => (
+             <li key={name}><strong>Razón social</strong>: {value.toString()}</li>
+           ))}
+         </ul>
+         <ul>
+           {Object.entries(nombreComercial).map(([name, value]) => (
+             <li key={name}><strong>Nombre Comercial</strong>: {value.toString()}</li>
+           ))}
+         </ul>
+         <ul>
+           {Object.entries(ruc).map(([name, value]) => (
+             <li key={name}><strong>RUC</strong>: {value.toString()}</li>
+           ))}
+         </ul>
+         <ul>
+           {Object.entries(contacto).map(([name, value]) => (
+             <li key={name}><strong>Nombre de Contacto</strong>: {value.toString()}</li>
+           ))}
+         </ul>
+         <ul>
+           {Object.entries(telefono).map(([name, value]) => (
+             <li key={name}><strong>Telefono/Celular</strong>: {value.toString()}</li>
+           ))}
+         </ul>
+       </div>
+      }
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <label>
+            <p>Razón Social</p>
+            <input name="name" onChange={handleChange}/>
+          </label>
+          <label>
+            <p>Nombre Comercial</p>
+            <input name="ape" onChange={handleChange2}/>
+          </label>
+
+          <label>
+            <p>RUC</p>
+            <input name="dir" onChange={handleChange3}/>
+          </label>
+          <label>
+            <p>Contacto</p>
+            <input name="ape" onChange={handleChange4}/>
+          </label>
+
+          <label>
+            <p>Telefono/Celular</p>
+            <input name="dir" onChange={handleChange5} />
+          </label>
+        </fieldset>
+        
+        <button type="submit">Registrar</button>
+      </form>
     </div>
-  );
+  )
 }
 
 export default App;
